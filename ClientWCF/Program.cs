629833e449c18dc05823aa8d6e904c69a94280d6
Davide Luppoli <davide@davideluppoli.it>
2024-11-27 12:53:50 +0100

    Add project files.

---
+++ ClientWCF/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientWCF.Olypics;
+using ClientWCF.ServiceReference;
+
+namespace ClientWCF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserire il codice atleta");
+            int codice = int.Parse(Console.ReadLine()); 
+
+            OlympicsClient client = new OlympicsClient();
+            Athlete result = client.GetAthlete(codice);
+
+            Console.WriteLine(result.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ServiceWCF/App_Code/ModelExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+
+[MetadataType(typeof(AthletesMetadata))]
+public partial class Athlete
+{
+
+}
+
+[DataContract]
+public class AthletesMetadata
+{
+    [DataMember]
+    public int IdAthlete { get; set; }
+    [DataMember]
+    public string Name { get; set; }
+    [DataMember]
+    public string Sex { get; set; }
+    [DataMember]
+    public Nullable<short> Height { get; set; }
+    [DataMember]
+    public Nullable<short> Weight { get; set; }
+}
